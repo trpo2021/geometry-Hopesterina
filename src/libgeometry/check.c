8347978fd3efa4becff3896b21b7b439351f0092
Hopesterina <elizovan@yandex.ry.noreply.github.com>
2021-03-19 13:06:22 +0700

    Update file check.c

--- src/libgeometry/check.c
+++ src/libgeometry/check.c
@@ -6,71 +6,63 @@ int name_verification(char* input_arr)
 {
     char name_of_figure[6] = "Circle";
     int k = 0;
-    for(int i = 0; i < 6; i++)
-    {
-        if(input_arr[i] != name_of_figure[i])
-        {
+    for (int i = 0; i < 6; i++) {
+        if (input_arr[i] != name_of_figure[i]) {
             k = k + 1;
             break;
         }
-    }     
+    }
     return k;
 }
 
 int checking_parameters_figure(char* input_arr)
 {
-    int j = 7;     //индекс значения после "("
-    int t = 0;     //вспомогательная переменная
+    int j = 7; //индекс значения после "("
+    int t = 0; //вспомогательная переменная
 
-    if (input_arr[j] == ' ')
-    {
+    if (input_arr[j] == ' ') {
         t++;
-    }                                       
-    while (input_arr[j] != ' ')
-    {
-        if ((input_arr[j] != '-') && (!(((input_arr[j] <= '9') && (input_arr[j] >= '0')) || (input_arr[j] == '.'))) && (input_arr[j] != ' '))
-        {
+    }
+    while (input_arr[j] != ' ') {
+        if ((input_arr[j] != '-')
+            && (!(((input_arr[j] <= '9') && (input_arr[j] >= '0'))
+                  || (input_arr[j] == '.')))
+            && (input_arr[j] != ' ')) {
             t++;
-            break;      
-        }
-        else
-        {    
+            break;
+        } else {
             j++;
-        }    
+        }
     }
 
-    while ((input_arr[j] != ',') && (input_arr[j + 1] != ' '))
-    {
-        if ((input_arr[j] != '-') && (!(((input_arr[j] <= '9') && (input_arr[j] >= '0')) || (input_arr[j] == '.'))) && (input_arr[j] != ' '))
-        {
+    while ((input_arr[j] != ',') && (input_arr[j + 1] != ' ')) {
+        if ((input_arr[j] != '-')
+            && (!(((input_arr[j] <= '9') && (input_arr[j] >= '0'))
+                  || (input_arr[j] == '.')))
+            && (input_arr[j] != ' ')) {
             t++;
-            break;         
-        }
-        else
-        {
+            break;
+        } else {
             j++;
-        }       
-    
-    } 
- 
-    if (input_arr[j + 1] != ' ')
-    {
+        }
+    }
+
+    if (input_arr[j + 1] != ' ') {
         t++;
     }
 
     j++;
- 
-    while (input_arr[j] != ')')
-    {
-        if ((input_arr[j] != '-') && (!(((input_arr[j] <= '9') && (input_arr[j] >= '0')) || (input_arr[j] == '.'))) && (input_arr[j] != ' '))
-        {
+
+    while (input_arr[j] != ')') {
+        if ((input_arr[j] != '-')
+            && (!(((input_arr[j] <= '9') && (input_arr[j] >= '0'))
+                  || (input_arr[j] == '.')))
+            && (input_arr[j] != ' ')) {
             t++;
-            break;          
-        }
-        else
-        { 
+            break;
+        } else {
             j++;
-        }            
+        }
     }
     return t;
 }
