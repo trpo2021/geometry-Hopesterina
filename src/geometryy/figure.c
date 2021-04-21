87bcaa23039cda237ac3c2201dab80657b3c0ec0
Hopesterina <elizovan@yandex.ry.noreply.github.com>
2021-04-22 02:03:58 +0700

    Update figure.c

--- src/geometryy/figure.c
+++ src/geometryy/figure.c
@@ -1,74 +1,93 @@
+#include "libgeometry/for_checking.h" //
 #include <ctype.h>
-#include <libgeometry/for_checking.h>
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
-#define N 50
+#define N 30 // максимальная длина строки
 
 int main()
 {
-    float radius = 0;
+    struct circle data_circle[50]; // массив структур для записи всех параметров
+                                   // кругов
+    float r = 0;                   // радиус
     float area = 0, perimetr = 0;
-    char r_arr[N];
-    char input_arr[N] = {0};
-
+    int ncircle = 0; // количество кругов
     FILE* fptr = NULL;
 
-    fptr = fopen("input.txt", "r");
+    fptr = fopen("bin/input.txt", "r");
     if (!fptr) {
         printf("Can`t open the file\n");
     } else {
-        int i = 0;
+        while (!feof(fptr)) //проход до конца файла
+        {
+            char r_arr[10] = {0};
+            char x_arr[10] = {0}; // зануляем параметры фигуры
+            char y_arr[10] = {0};
+            char input_arr[N]
+                    = {0}; //каждую итерацию зануляем массив, для новой строчки
+            fgets(input_arr, 30, fptr);
+            printf("\n");
 
-        while (i < N) {
-            fscanf(fptr, "%c", &input_arr[i]);
-            ++i;
-        }
-    }
+            int counter_1 = name_verification(input_arr);
+            int counter_2 = checking_parameters_figure(input_arr);
 
-    fclose(fptr); //записали строку из файла в массив а
+            if (counter_1 > 0) {
+                for (int i = 0; i < N; i++) {
+                    printf("%c", input_arr[i]);
+                }
+                printf("incorrect name of figure\n");
+                printf("please correct\n"); // ошибка об имени
 
-    int counter_1 = name_verification(input_arr);
-    int counter_2 = checking_parameters_figure(input_arr);
-
-    if (counter_1 > 0) {
-        for (int i = 0; i < N; i++) {
-            printf("%c", input_arr[i]);
-        }
-        printf("incorrect name of figure\n");
-        printf("please correct\n"); // ошибка об имени
-
-    } else {
-        if (counter_2 > 0) {
-            for (int i = 0; i < N; i++) {
-                printf("%c", input_arr[i]);
-            }
-            printf("invalid values\n");
-            printf("please correct\n"); // ошибка о параметрах фигуры
-        } else {
-            for (int i = 0; i < N; i++) {
-                if ((input_arr[i] == ',') && (input_arr[i + 1] == ' ')) {
-                    for (int k = i; input_arr[k + 1] != ')'; k++) {
-                        r_arr[k - i] = input_arr[k + 1];
+            } else {
+                if (counter_2 > 0) {
+                    for (int i = 0; i < N; i++) {
+                        printf("%c", input_arr[i]);
                     }
+                    printf("invalid values\n");
+                    printf("please correct\n"); // ошибка о параметрах фигуры
+                } else {
+                    for (int i = 0; i < N; i++) {
+                        if ((input_arr[i] == ',')
+                            && (input_arr[i + 1] == ' ')) { // поиск радиуса
+                            for (int k = i; input_arr[k + 1] != ')'; k++) {
+                                r_arr[k - i] = input_arr[k + 1];
+                            }
+                        }
+                        if (input_arr[i] == '(') { // поиск x
+                            for (int k = i; input_arr[k + 1] != ' '; k++) {
+                                x_arr[k - i] = input_arr[k + 1];
+                            }
+                        }
+                        if (input_arr[i] == ' ') { // поиск y
+                            for (int k = i - 1; input_arr[k] != ','; k++) {
+                                y_arr[k - i] = input_arr[k];
+                            }
+                        }
+                    }
+                    ++ncircle;
+                    r = atof(r_arr);
+                    area = area_f(r);
+                    perimetr = perimetr_f(r);
+
+                    printf("%d. ", ncircle);
+                    for (int i = 0; i < N; i++) {
+                        printf("%c", input_arr[i]);
+                    }
+                    printf("area = %.2f\n", area);
+                    printf("perimeter = %.2f\n", perimetr);
+
+                    data_circle[ncircle].r = r;
+                    data_circle[ncircle].x = atoi(x_arr);
+                    data_circle[ncircle].y = atoi(y_arr);
                 }
             }
-
-            radius = atof(r_arr);
-
-            area = M_PI * radius * radius;
-
-            perimetr = 2 * M_PI * radius;
-
-            printf("\n");
-            for (int i = 0; i < N; i++) {
-                printf("%c", input_arr[i]);
-            }
-
-            printf("area = %.3f\n", area);
-            printf("perimeter = %.3f\n", perimetr);
         }
     }
+
+    fclose(fptr);
+    printf("\n");
+    printf("----------------------------\n");
+    print_intersects(ncircle, data_circle);
 
     return 0;
 }
